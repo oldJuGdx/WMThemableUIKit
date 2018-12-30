8728b498bb8333ebc003e5fdeaf695e7af6f7b85
Goudeaux Julien <julien.goudeaux@weamo.fr>
2018-12-30 22:31:39 +0100

    Workspace creation

---
+++ WMThemableUIKIt/Interface/WMThemableUIKIt.swift
@@ -0,0 +1,13 @@
+//
+//  WMThemableUIKIt.swift
+//  WMThemableUIKIt
+//
+//  Created by Julien Goudeaux on 30/12/2018.
+//  Copyright © 2018 Weamo. All rights reserved.
+//
+
+import Foundation
+
+public protocol WMThemableUIKit {
+	
+}
