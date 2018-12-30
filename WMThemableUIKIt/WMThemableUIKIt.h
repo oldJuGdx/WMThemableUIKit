9a14337195cd627c3afbf01ab0122460b4795d33
Goudeaux Julien <julien.goudeaux@weamo.fr>
2018-12-30 18:22:46 +0100

    Framework creation

---
+++ WMThemableUIKIt/WMThemableUIKIt.h
@@ -0,0 +1,19 @@
+//
+//  WMThemableUIKIt.h
+//  WMThemableUIKIt
+//
+//  Created by Julien Goudeaux on 30/12/2018.
+//  Copyright © 2018 Weamo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WMThemableUIKIt.
+FOUNDATION_EXPORT double WMThemableUIKItVersionNumber;
+
+//! Project version string for WMThemableUIKIt.
+FOUNDATION_EXPORT const unsigned char WMThemableUIKItVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WMThemableUIKIt/PublicHeader.h>
+
+
